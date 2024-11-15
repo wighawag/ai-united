6aef165af5b585af13de128c4771ee3e41986de4
wighawag <wighawag@gmail.com>
2024-11-15 23:01:06 +0700

    add machine executor + wasm2hex

---
+++ machine-executor/src/main.rs
@@ -0,0 +1,87 @@
+use core::str;
+use std::slice;
+
+use clap::Parser;
+use lz4_flex::decompress;
+use machine::Executor;
+
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::BufReader;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct WasmData {
+    wasm: String,
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Input file to compress and convert
+    #[arg(short, long)]
+    module: String,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let payload: &str = &cli.module.as_str();
+
+    println!("payload: {}", payload);
+
+    let compressed_bytes = if payload.starts_with("0x") {
+        let s = unsafe {
+            // First, we build a &[u8]...
+            let slice = slice::from_raw_parts(payload.as_ptr().add(2), payload.len() - 2);
+
+            // ... and then convert that slice into a string slice
+            str::from_utf8(slice)
+        }
+        .expect("failed to slice payload");
+
+        hex::decode(s).expect("failed to decode payload")
+    } else {
+        // Open the file
+        let file = File::open(payload).expect("failed to read file");
+        let reader = BufReader::new(file);
+        // Parse the JSON
+        let wasm_data: WasmData = serde_json::from_reader(reader).expect("fauled to parse json");
+
+        let s = unsafe {
+            // First, we build a &[u8]...
+            let slice =
+                slice::from_raw_parts(wasm_data.wasm.as_ptr().add(2), wasm_data.wasm.len() - 2);
+
+            // ... and then convert that slice into a string slice
+            str::from_utf8(slice)
+        }
+        .expect("failed to slice payload");
+
+        hex::decode(s).expect("failed to decode payload")
+    };
+
+    let mut wasm_bytes_1 = if let Ok(b) = decompress(&compressed_bytes, 1000000) {
+        b
+    } else {
+        println!("could not decompress, assume it is not compressed");
+        compressed_bytes.clone()
+    };
+    let wasm_bytes_1: &mut [u8] = &mut wasm_bytes_1; //cast to `&mut [u8]`
+
+    let mut wasm_bytes_2 = if let Ok(b) = decompress(&compressed_bytes, 1000000) {
+        b
+    } else {
+        println!("could not decompress, assume it is not compressed");
+        compressed_bytes.clone()
+    };
+    let wasm_bytes_2: &mut [u8] = &mut wasm_bytes_2; //cast to `&mut [u8]`
+
+    let mut executor = Executor::new();
+    println!("addding module 1 ...");
+    executor.add_module(wasm_bytes_1);
+    println!("addding module 2 ...");
+    executor.add_module(wasm_bytes_2);
+
+    println!("execute!");
+    executor.execute();
+}
---
+++ wasm2hex/src/main.rs
@@ -0,0 +1,71 @@
+use clap::Parser;
+use lz4_flex::compress;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::PathBuf;
+
+#[derive(Serialize, Deserialize)]
+struct WasmModule {
+    wasm: String,
+    uncompressed: String,
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Input file to compress and convert
+    #[arg(short, long)]
+    input: PathBuf,
+
+    /// Output file to save the hex string
+    #[arg(short, long)]
+    output: PathBuf,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // Read input file
+    let mut input_file = File::open(&cli.input).expect("failed to open file");
+    let mut input_data = Vec::new();
+    input_file
+        .read_to_end(&mut input_data)
+        .expect("failed to read file");
+
+    let uncompressed_hex = {
+        // Convert compressed data to hex string
+        let mut hex_string = hex::encode(&input_data);
+        hex_string.insert_str(0, "0x");
+        hex_string
+    };
+    println!("{}", uncompressed_hex);
+
+    // Compress the data
+    // let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+    // encoder.write_all(&input_data)?;
+    // let compressed_data = encoder.finish()?;
+    let compressed_data = compress(&input_data);
+
+    let module = {
+        // Convert compressed data to hex string
+        let mut hex_string = hex::encode(&compressed_data);
+        hex_string.insert_str(0, "0x");
+        WasmModule {
+            wasm: hex_string,
+            uncompressed: uncompressed_hex,
+        }
+    };
+
+    // Serialize it to a JSON string.
+    let json = serde_json::to_string(&module).expect("failed to serialize");
+
+    // Write hex string to output file
+    let mut output_file = File::create(&cli.output).expect("failed to create file");
+    output_file
+        .write_all(json.as_bytes())
+        .expect("faile dto write file");
+
+    println!("File compressed and converted to hex successfully!");
+    println!("{}", module.wasm);
+}
