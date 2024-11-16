467235282fcb8c61f39ca3d719b94686f3757ad0
wighawag <wighawag@gmail.com>
2024-11-17 03:39:32 +0700

    fix

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-	let { children } = $props();
+	// let { children } = $props();
 </script>
 
-{@render children()}
+<!-- {@render children()} -->
+<!-- required due to wasm issue : https://github.com/sveltejs/svelte/issues/13155-->
+<slot />
