45e35fb8df10b8402ee6acefc71d78864828361c
wighawag <wighawag@gmail.com>
2024-11-16 14:21:42 +0700

    wip: hookup physics for bots

--- machine/src/lib.rs
+++ machine/src/lib.rs
@@ -1,3 +1,6 @@
+use nalgebra::ArrayStorage;
+use nalgebra::Const;
+use nalgebra::Matrix;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::wasm_bindgen;
 #[cfg(target_arch = "wasm32")]
@@ -55,13 +58,14 @@ struct BotModule {
     store: Store,
     init: TypedFunction<u32, ()>,
     compute_actions: TypedFunction<(u32, u32, u32, u32, u32, u32), u32>,
+    handle: RigidBodyHandle,
 }
 
 const INIT_GAS: u64 = 1_000;
 const COMPUTE_ACTIONS_GAS: u64 = 1_000_000;
 const MAX_NUM_UPDATES: u64 = 1_000_000;
 
-fn create_bot_module(wasm_bytes: &mut [u8]) -> BotModule {
+fn create_bot_module(wasm_bytes: &mut [u8], handle: RigidBodyHandle) -> BotModule {
     #[cfg(not(target_arch = "wasm32"))]
     let mut store = {
         // Let's define our cost function.
@@ -150,6 +154,7 @@ fn create_bot_module(wasm_bytes: &mut [u8]) -> BotModule {
         store,
         compute_actions,
         init,
+        handle,
     }
 }
 
@@ -284,26 +289,100 @@ impl BotModule {
 pub struct Battle {
     bot1: Option<BotModule>,
     bot2: Option<BotModule>,
+    collider_set: ColliderSet,
+    physics_pipeline: PhysicsPipeline,
+    rigid_body_set: RigidBodySet,
+    gravity: Matrix<f32, Const<2>, Const<1>, ArrayStorage<f32, 2, 1>>,
+    integration_parameters: IntegrationParameters,
+    island_manager: IslandManager,
+    broad_phase: BroadPhaseMultiSap,
+    narrow_phase: NarrowPhase,
+    impulse_joint_set: ImpulseJointSet,
+    multibody_joint_set: MultibodyJointSet,
+    ccd_solver: CCDSolver,
+    query_pipeline: QueryPipeline,
+    physics_hooks: (),
+    event_handler: (),
+    ball: RigidBodyHandle,
 }
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 impl Battle {
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen(constructor))]
     pub fn new() -> Battle {
+        let mut rigid_body_set = RigidBodySet::new();
+        let mut collider_set = ColliderSet::new();
+
+        /* Create the ground. */
+        let collider = ColliderBuilder::cuboid(100.0, 0.1).build();
+        collider_set.insert(collider);
+
+        /* Create other structures necessary for the simulation. */
+        let gravity = vector![0.0, -9.81];
+        let integration_parameters = IntegrationParameters::default();
+        let physics_pipeline = PhysicsPipeline::new();
+        let island_manager = IslandManager::new();
+        let broad_phase = DefaultBroadPhase::new();
+        let narrow_phase = NarrowPhase::new();
+        let impulse_joint_set = ImpulseJointSet::new();
+        let multibody_joint_set = MultibodyJointSet::new();
+        let ccd_solver = CCDSolver::new();
+        let query_pipeline = QueryPipeline::new();
+        let physics_hooks = ();
+        let event_handler = ();
+
+        /* Create the bouncing ball. */
+        let rigid_body = RigidBodyBuilder::dynamic()
+            .translation(vector![0.0, 10.0])
+            .build();
+        let collider = ColliderBuilder::ball(0.5).restitution(0.7).build();
+        let ball = rigid_body_set.insert(rigid_body);
+        collider_set.insert_with_parent(collider, ball, &mut rigid_body_set);
+
         Battle {
             bot1: None,
             bot2: None,
+            collider_set,
+            physics_pipeline,
+            rigid_body_set,
+            gravity,
+            integration_parameters,
+            island_manager,
+            broad_phase,
+            narrow_phase,
+            impulse_joint_set,
+            multibody_joint_set,
+            ccd_solver,
+            query_pipeline,
+            physics_hooks,
+            event_handler,
+            ball,
         }
     }
+
+    fn create_bot_handle(&mut self) -> RigidBodyHandle {
+        /* Create the bouncing ball. */
+        let rigid_body = RigidBodyBuilder::dynamic()
+            .translation(vector![0.0, 10.0])
+            .build();
+        let collider = ColliderBuilder::ball(0.5).restitution(0.7).build();
+        let handle = self.rigid_body_set.insert(rigid_body);
+        self.collider_set
+            .insert_with_parent(collider, handle, &mut self.rigid_body_set);
+        handle
+    }
+
     pub fn add_bot(&mut self, wasm_bytes: &mut [u8]) {
         if let Some(_existing_bot) = &self.bot1 {
             if let Some(_existing_bot) = &self.bot2 {
                 panic!("already both bots added");
             } else {
-                self.bot2 = Some(create_bot_module(wasm_bytes));
+                let handle = self.create_bot_handle();
+                self.bot2 = Some(create_bot_module(wasm_bytes, handle));
             }
         } else {
-            self.bot1 = Some(create_bot_module(wasm_bytes));
+            let handle = self.create_bot_handle();
+            self.bot1 = Some(create_bot_module(wasm_bytes, handle));
         }
     }
 
@@ -331,56 +410,25 @@ impl Battle {
         bot1.compute_actions(0, 0, 0, 0, 0, 0);
         bot2.compute_actions(0, 0, 0, 0, 0, 0);
 
-        let mut rigid_body_set = RigidBodySet::new();
-        let mut collider_set = ColliderSet::new();
-
-        /* Create the ground. */
-        let collider = ColliderBuilder::cuboid(100.0, 0.1).build();
-        collider_set.insert(collider);
-
-        /* Create the bouncing ball. */
-        let rigid_body = RigidBodyBuilder::dynamic()
-            .translation(vector![0.0, 10.0])
-            .build();
-        let collider = ColliderBuilder::ball(0.5).restitution(0.7).build();
-        let ball_body_handle = rigid_body_set.insert(rigid_body);
-        collider_set.insert_with_parent(collider, ball_body_handle, &mut rigid_body_set);
-
-        /* Create other structures necessary for the simulation. */
-        let gravity = vector![0.0, -9.81];
-        let integration_parameters = IntegrationParameters::default();
-        let mut physics_pipeline = PhysicsPipeline::new();
-        let mut island_manager = IslandManager::new();
-        let mut broad_phase = DefaultBroadPhase::new();
-        let mut narrow_phase = NarrowPhase::new();
-        let mut impulse_joint_set = ImpulseJointSet::new();
-        let mut multibody_joint_set = MultibodyJointSet::new();
-        let mut ccd_solver = CCDSolver::new();
-        let mut query_pipeline = QueryPipeline::new();
-        let physics_hooks = ();
-        let event_handler = ();
-
         /* Run the game loop, stepping the simulation once per frame. */
-        for _ in 0..3 {
-            physics_pipeline.step(
-                &gravity,
-                &integration_parameters,
-                &mut island_manager,
-                &mut broad_phase,
-                &mut narrow_phase,
-                &mut rigid_body_set,
-                &mut collider_set,
-                &mut impulse_joint_set,
-                &mut multibody_joint_set,
-                &mut ccd_solver,
-                Some(&mut query_pipeline),
-                &physics_hooks,
-                &event_handler,
-            );
+        self.physics_pipeline.step(
+            &self.gravity,
+            &self.integration_parameters,
+            &mut self.island_manager,
+            &mut self.broad_phase,
+            &mut self.narrow_phase,
+            &mut self.rigid_body_set,
+            &mut self.collider_set,
+            &mut self.impulse_joint_set,
+            &mut self.multibody_joint_set,
+            &mut self.ccd_solver,
+            Some(&mut self.query_pipeline),
+            &self.physics_hooks,
+            &self.event_handler,
+        );
 
-            let ball_body = &rigid_body_set[ball_body_handle];
-            println!("Ball altitude: {}", ball_body.translation().y);
-        }
+        let ball_body = &self.rigid_body_set[self.ball];
+        println!("Ball altitude: {}", ball_body.translation().y);
 
         // TODO ball in camp
         0
