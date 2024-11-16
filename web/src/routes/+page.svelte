26b4407b85f97f716aa3e46f9303bf965eac1ba8
wighawag <wighawag@gmail.com>
2024-11-16 11:42:32 +0700

    use wasm-pack default + vite plugin for wasm

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { dummy_test } from "$lib";
+	import { dummy_test } from '$lib';
 
-    function test() {
-        dummy_test();
-    }
+	function test() {
+		dummy_test();
+	}
 </script>
+
 <button onclick={() => test()}>Dummy Test</button>
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), wasm(), topLevelAwait()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
