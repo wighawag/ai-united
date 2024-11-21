d76aba4626052c5cdcbfdc0a125d7d7183c48077
wighawag <wighawag@gmail.com>
2024-11-21 20:57:50 +0000

    example bot link

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -3,3 +3,13 @@
 </script>
 
 <DropZone></DropZone>
+
+<div class="download-example">
+	<a href="/bot_rust.wasm" class="underline">download example bot wasm file</a>
+</div>
+
+<style>
+	.download-example {
+		margin: 2em;
+	}
+</style>
